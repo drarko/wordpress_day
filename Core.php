362457c6185b8146e603be889d86b951588c4103
gshokida <german.shokida@gmail.com>
2016-09-13 10:51:35 -0300

    Todo pago

--- Core.php
+++ Core.php
@@ -8,17 +8,17 @@ class Core {
 	private $sdk;
 
 	public function __construct() {
-		do_action("todoapgo_pre_create_sdk");
+		do_action("todopago_pre_create_sdk");
 		
 		$http_header = apply_filters("todopago_header","");
 		$mode = apply_filters("todopago_mode","test");
 		
 		$this->sdk = new TodoPago\Sdk($http_header, $mode);
-		do_action("todoapgo_post_create_sdk");
+		do_action("todopago_post_create_sdk");
 	}
 
 	public function call_sar() {
-		do_action("todoapgo_pre_call_sar");	
+		do_action("todopago_pre_call_sar");	
 
 		$http_header = apply_filters("todopago_sar_data_comercio",array());
 		$mode = apply_filters("todopago_sar_data_operacion",array());
@@ -27,7 +27,7 @@ class Core {
 		do_action("todopago_response_sar",$responseSAR);
 		//add_action("todopago_response_sar", "mi_responsesar_processor",0,2);
 
-		do_action("todoapgo_post_call_sar");	
+		do_action("todopago_post_call_sar");	
 	}
 
 	//etc... etc...
